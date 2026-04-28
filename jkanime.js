b16bea0ffb1d843c831da0cfa904ef073288de94
Dollxar <german.reyes22@cetis107.edu.mx>
2026-04-28 00:26:34 -0700

    hola

---
+++ jkanime.js
@@ -0,0 +1,29 @@
+export default {
+  id: "jkanime",
+  name: "JKanime",
+
+  async search(query) {
+    return [
+      {
+        title: "Prueba funcionando: " + query,
+        url: "https://jkanime.net"
+      }
+    ];
+  },
+
+  async episodes(url) {
+    return [
+      {
+        number: 1,
+        title: "Episodio 1",
+        url: "https://jkanime.net"
+      }
+    ];
+  },
+
+  async stream(url) {
+    return {
+      url: "https://jkanime.net"
+    };
+  }
+};
